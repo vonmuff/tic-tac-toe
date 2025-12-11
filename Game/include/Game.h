7758534c130a8f911ec70ddc5afb7344acbfed49
Muff <muffmuffin0501@gmail.com>
2025-12-11 23:37:39 +0200

    Add directories CMakeLists files and the Game class

---
+++ Game/include/Game.h
@@ -0,0 +1,13 @@
+//
+// Created by user on 11.12.2025.
+//
+
+#ifndef TICTACHOE_GAME_H
+#define TICTACHOE_GAME_H
+
+
+class Game {
+};
+
+
+#endif //TICTACHOE_GAME_H
---
+++ Game/source/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 11.12.2025.
+//
+
+#include "../include/Game.h"
